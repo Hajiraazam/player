8bff7ec7a639b841c3efdd6a6cba8e6225d2f036
Hajiraazam <hajiraazam246@gmail.com>
2025-09-22 09:50:37 +0500

    depensencies

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import connectDB from './db/index.js';
-connectDB()
+import {app} from "./app.js"
 dotenv.config({
   path : './env'
+})
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+       console.log("Error:" , error);
+    })
+    const PORT = process.env.PORT || 4001
+  app.listen(PORT,()=>{
+    console.log("Server is listening at PORT: ", PORT)
+  })
+})
+.catch((err)=>{
+  console.log("MONGO_DB connection failed : " , err)
 })
 
 
