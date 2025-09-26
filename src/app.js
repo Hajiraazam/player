b138ad4f8c7d26361d9fadb0af768af78f0fb865
Hajiraazam <hajiraazam246@gmail.com>
2025-09-26 20:16:44 +0500

    controllers and routes

--- src/app.js
+++ src/app.js
@@ -11,4 +11,7 @@ app.use(express.json({limit : "16kb"}));
 app.use(express.urlencoded({extended:true,limit : "16kb" }));
 app.use(express.static('public')); 
 app.use(cookieParser());
+
+// Routes import
+import userRouter from './routes/user.route.js'
 export {app}
